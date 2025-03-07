e48512cdc9479af89efdde36010ba288683f29ec
kimehyun <lh7721004@gmail.com>
2025-03-07 18:39:14 +0900

    feat: 통장 엔터티 추가

--- src/main/java/com/example/dgu_semi_erp_back/entity/Bankbook/Bankbook.java
+++ src/main/java/com/example/dgu_semi_erp_back/entity/Bankbook/Bankbook.java
@@ -1,4 +1,37 @@
 package com.example.dgu_semi_erp_back.entity.Bankbook;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Bankbook {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int bankbook_id;
+
+    @JoinColumn(name = "club_id")
+    private int club_id;
+
+    //통장 소유자
+    @Column(nullable = false)
+    private int owner;
+
+    //계좌번호
+    @Column(nullable = false)
+    private int number;
+
+    @Column(nullable = false)
+    private LocalDateTime create_at;
+
+
+
 }
