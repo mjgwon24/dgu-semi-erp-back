29cbd4d3427ad869421f78d36ea18364c757d8a5
mjgwon24 <alswlchlrh8@naver.com>
2025-04-05 11:50:28 +0900

    update: Enum 타입 컬럼 어노테이션 추가 및 @EnableJpaAuditing 추가

--- src/main/java/com/example/dgu_semi_erp_back/DguSemiErpBackApplication.java
+++ src/main/java/com/example/dgu_semi_erp_back/DguSemiErpBackApplication.java
@@ -2,7 +2,9 @@ package com.example.dgu_semi_erp_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DguSemiErpBackApplication {
 
