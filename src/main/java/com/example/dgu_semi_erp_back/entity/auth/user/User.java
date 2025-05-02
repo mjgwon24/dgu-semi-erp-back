8c7ba35bd70aca0fe101fc984605eb1da397e47c
김이현 <lh7721004@gmail.com>
2025-05-03 05:43:19 +0900

    update: userRole 추가

--- src/main/java/com/example/dgu_semi_erp_back/entity/auth/user/User.java
+++ src/main/java/com/example/dgu_semi_erp_back/entity/auth/user/User.java
@@ -4,7 +4,6 @@ import com.example.dgu_semi_erp_back.common.support.BaseEntity;
 import com.example.dgu_semi_erp_back.dto.user.UserCommandDto.UserUpdateRequest;
 import com.example.dgu_semi_erp_back.entity.account.Account;
 import com.example.dgu_semi_erp_back.entity.club.ClubMember;
-import com.example.dgu_semi_erp_back.entity.club.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
@@ -44,6 +43,10 @@ public class User extends BaseEntity {
     @ColumnDefault("false")
     @Column(columnDefinition = "TINYINT(1)")
     private boolean isVerified; // 이메일 인증 여부
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Account> accounts;
