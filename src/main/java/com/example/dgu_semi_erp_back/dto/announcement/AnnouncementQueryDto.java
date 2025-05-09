80007cfcd0b834ea4199874793fd01baa8822a99
Hj99Jeon <wole1115@naver.com>
2025-05-10 02:28:08 +0900

    Announcement List 조회

--- src/main/java/com/example/dgu_semi_erp_back/dto/announcement/AnnouncementQueryDto.java
+++ src/main/java/com/example/dgu_semi_erp_back/dto/announcement/AnnouncementQueryDto.java
@@ -1,5 +1,29 @@
 package com.example.dgu_semi_erp_back.dto.announcement;
 
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public final class AnnouncementQueryDto {
     private AnnouncementQueryDto() {}
+
+    @Builder
+    public record AnnouncementSummariesListResponse(
+            List<AnnouncementSummaryResponse> content,
+            Integer pageNumber,
+            Integer pageSize,
+            Long totalElements,
+            Integer totalPages,
+            Boolean last
+    ) {
+        @Builder
+        public record AnnouncementSummaryResponse(
+                Long id,
+                String title,
+                String content,
+                LocalDateTime createdAt,
+                LocalDateTime updatedAt
+        ) {}
+    }
 }
--- src/main/java/com/example/dgu_semi_erp_back/service/announcement/AnnouncementQueryService.java
+++ src/main/java/com/example/dgu_semi_erp_back/service/announcement/AnnouncementQueryService.java
@@ -3,19 +3,38 @@ package com.example.dgu_semi_erp_back.service.announcement;
 import com.example.dgu_semi_erp_back.common.exception.CustomException;
 import com.example.dgu_semi_erp_back.common.exception.ErrorCode;
 import com.example.dgu_semi_erp_back.entity.announcement.Announcement;
+import com.example.dgu_semi_erp_back.mapper.AnnouncementDtoMapper;
+import com.example.dgu_semi_erp_back.projection.announcement.AnnouncementProjection.AnnouncementSummary;
 import com.example.dgu_semi_erp_back.repository.announcement.AnnouncementQueryRepository;
 import com.example.dgu_semi_erp_back.usecase.announcement.AnnouncementUseCase;
+import com.example.dgu_semi_erp_back.usecase.announcement.FindAnnouncementSummariesUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
-public class AnnouncementQueryService implements AnnouncementUseCase {
+public class AnnouncementQueryService implements AnnouncementUseCase, FindAnnouncementSummariesUseCase {
     private final AnnouncementQueryRepository announcementRepository;
+    private final AnnouncementDtoMapper announcementDtoMapper;
 
     @Override
     public Announcement findAnnouncementById(Long id) {
         return announcementRepository.findAnnouncementById(id)
                 .orElseThrow(() -> new CustomException(ErrorCode.ANNOUNCEMENT_NOT_FOUND));
+    }
+
+    @Override
+    public Page<AnnouncementSummary> findAnnouncementSummaries(
+            Pageable pageable,
+            String title,
+            String content,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt
+    ) {
+        return announcementRepository.findAnnouncementSummariesBy(pageable);
     }
 }
