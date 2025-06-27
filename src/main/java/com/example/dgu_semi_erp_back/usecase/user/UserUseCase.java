fbb50e8aeef908a04ce5663aba72bd4d88ee6878
kimehyun <lh7721004@gmail.com>
2025-06-27 19:40:36 +0900

    update: 동아리 목록 조회 API 조건 필터링 개선

--- src/main/java/com/example/dgu_semi_erp_back/usecase/user/UserUseCase.java
+++ src/main/java/com/example/dgu_semi_erp_back/usecase/user/UserUseCase.java
@@ -7,6 +7,8 @@ import com.example.dgu_semi_erp_back.dto.user.UserCommandDto.UserResponse;
 import com.example.dgu_semi_erp_back.dto.user.UserCommandDto.UserRoleUpdateRequest;
 import com.example.dgu_semi_erp_back.dto.user.UserCommandDto.UserEmailUpdateRequest;
 import com.example.dgu_semi_erp_back.entity.auth.user.User;
+import com.example.dgu_semi_erp_back.entity.club.ClubStatus;
+import com.example.dgu_semi_erp_back.entity.club.MemberStatus;
 import com.example.dgu_semi_erp_back.projection.club.ClubProjection.ClubSummary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,5 +19,21 @@ public interface UserUseCase {
     User updateRole(Long id,UserRoleUpdateRequest request,String username);
     User updateEmail(Long userId, UserEmailUpdateRequest request,String username);
     User findUserByAccessToken(String accessToken);
-    UserClubMemberDto.ClubMemberSearchResponse getUserClubs(String accessToken, Pageable pageable);
+    UserClubMemberDto.ClubMemberSearchResponse getUserClubs(
+            String accessToken,
+            Long currentPeopleMin,
+            Long currentPeopleMax,
+            Long totalPeopleMin,
+            Long totalPeopleMax,
+            String clubName,
+            ClubStatus status,
+            Pageable pageable);
+    UserClubMemberDto.ClubSearchResponse getAllClubs(
+            String username,
+            Long currentPeopleMin,
+            Long currentPeopleMax,
+            Long totalPeopleMin,
+            Long totalPeopleMax,
+            String clubName,
+            ClubStatus status);
 }
